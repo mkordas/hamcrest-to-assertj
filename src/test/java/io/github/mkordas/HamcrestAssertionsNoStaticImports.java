fcc6cc374646d9198b79f10493ca4f8187ca9e82
Michal Kordas <kontakt@michalkordas.com>
2015-05-03 01:50:35 +0200

    Extend no static import samples

--- src/test/java/io/github/mkordas/HamcrestAssertionsNoStaticImports.java
+++ src/test/java/io/github/mkordas/HamcrestAssertionsNoStaticImports.java
@@ -4,6 +4,8 @@ import org.hamcrest.CoreMatchers;
 import org.hamcrest.Matchers;
 import org.hamcrest.core.Is;
 import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsNot;
+import org.hamcrest.core.IsNull;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,5 +16,7 @@ public class HamcrestAssertionsNoStaticImports {
         Assert.assertThat(2, IsEqual.equalTo(2));
         Assert.assertThat(3, Matchers.is(3));
         Assert.assertThat(4, CoreMatchers.is(4));
+        Assert.assertThat(5, IsNot.not(6));
+        Assert.assertThat(6, IsNull.notNullValue());
     }
 }
